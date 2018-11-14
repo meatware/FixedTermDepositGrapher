2ffb8734dc0cee1f546ecaddb7bbc86243f86d91
meatware <wetware@zoho.com>
2018-11-14 21:43:07 +0000

    Tweak form submission & rendering

--- ftd_main/routes.py
+++ ftd_main/routes.py
@@ -1,6 +1,6 @@
 from ftd_main import app, db
 from ftd_main.models import FixedDeposit
-from ftd_main.forms import AddFixedDepositForm, create_deposit
+from ftd_main.forms import AddFixedDepositForm, create_deposit, SubmitField
 from flask import flash, render_template, request, redirect
 
 @app.route('/')
@@ -15,16 +15,16 @@ def new_deposit():
     """
     Add a new deposit
     """
-    form = AddFixedDepositForm(request.form)
+    form = AddFixedDepositForm()
 
-    if request.method == 'POST' and form.validate():
-        # save the album
+    if form.validate_on_submit():
+        # save the deposit
         deposit = FixedDeposit()
         new_deposit = create_deposit(deposit, form, new=True)
         db.session.add(new_deposit)
         db.session.commit()
 
         flash('New Fixed Deposit created successfully!')
-        return redirect('/')
+        return redirect('/index')
 
-    return render_template('new_deposit.html', form=form)
+    return render_template('new_deposit.html',  title='Add Deposit', form=form)
