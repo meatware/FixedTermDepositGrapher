8605c8f21186be9dfd9f61ee1898f21b4666ff9f
Giri <wetware@zoho.com>
2018-11-25 14:34:43 +0000

    trying to get datepicker working

--- ftd_main/__init__.py
+++ ftd_main/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+#from flask_datepicker import datepicker
 
 ### Create App
 app = Flask(__name__)
@@ -14,6 +15,8 @@ migrate = Migrate(app, db)
 #
 login = LoginManager(app)
 login.login_view = 'login'
+#
+#date_pick = datepicker(app)
 
 
 ### package imports
