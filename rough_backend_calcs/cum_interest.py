04c1628e81700c9d462e5553b724251875605941
girifirm <girishtampi@firmstep.com>
2018-12-26 13:56:24 +0000

    did some tidying up and separating into functions - needs more though

--- rough_backend_calcs/cum_interest.py
+++ rough_backend_calcs/cum_interest.py
@@ -12,67 +12,90 @@ def string_to_datetime(in_str, fmt_def='%d/%m/%y'):
     dt_obj = datetime.strptime(in_str, fmt_def)
     return dt_obj
 
-def calculate_maturity(depo_dict):
-    #http://www.allbankingsolutions.com/fdcal.htm
-    # https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
-    pri = depo_dict["initial_deposit"]
-    irate = depo_dict["interest_rate"]/100
-    period = depo_dict["period"]
+def calc_freq(scheme_label):
 
-    # insert calcs
-    freq =  4.0  #depo_dict["initial_deposit"]
+    cum_freq = 1 # yearly
+    cum_days = 1
 
-    maturity_val = 0
-    #for _ in range(4):
-    power_calc = freq * (period/12)
-    maturity_val = pri * (1 + irate/freq)**power_calc
+    scheme_fmt = scheme_label.lower()
+    if "quart" in scheme_fmt:
+        cum_freq = 4
+        cum_days = 91.25
+    elif "month" in scheme_fmt:
+        cum_freq = 12
+        cum_days = 30 #??
 
-    print("\n\nref_maturity_Amount", depo_dict["ref_maturity_Amount"])
-    print("maturity_val", round(maturity_val, 1))
+    return {"cum_freq": cum_freq, "cum_days": cum_days}
 
-    print("\ndiff", round(depo_dict["ref_maturity_Amount"] - maturity_val, 2) )
-    #sys.exit(0)
+def print_calc_verific(ref_string, maturity_val, ref_maturity_Amount):
+
+    print("\n\nref_maturity_Amount", ref_maturity_Amount)
+    print(ref_string, maturity_val)
+
+    print("\ndelta", round(ref_maturity_Amount - maturity_val, 2) )
 
     return
+
+def calc_approx_maturity(depo_dict):
+    principal = depo_dict["initial_deposit"]
+    irate = depo_dict["interest_rate"]/100
+    month_period = depo_dict["period"]
+
+    # insert calcs
+    get_freq = calc_freq(depo_dict["scheme"])
+    freq = get_freq["cum_freq"]
+
+    power_calc = freq * (month_period/12)
+    maturity_val = principal * (1 + irate/freq)**power_calc
+
+    return round(maturity_val, 2)
 
 def calculate_daily_maturity(depo_dict):
     # https://en.wikipedia.org/wiki/Rounding#Round_half_to_even
     #https://money.stackexchange.com/questions/10796/daily-interest-calculation-combined-with-monthly-compounding-why-do-banks-do-th
-    pri = depo_dict["initial_deposit"]
+    principal = depo_dict["initial_deposit"]
     irate = depo_dict["interest_rate"]/100
-    period = depo_dict["period"] # months
+    month_period = depo_dict["period"]
+
     start_date = string_to_datetime(depo_dict["start_date"])
     end_date = string_to_datetime(depo_dict["ref_end_date"])
 
     day_delta = end_date - start_date
-    day_period = day_delta.days # in days
+    day_period = day_delta.days
     print("day_period", day_period)
-    print("dayx", (period/12)*365)
 
-    daily_irate = (depo_dict["interest_rate"]/100)/365
+    daily_irate = irate/365
     print("daily_irate", daily_irate)
 
     # figure out n left over compounding days
-    full_n_cmpds = floor(day_period/91.25) # in the case ogf quarterly compundsing 30x4
-    remain_cmpd_days = day_period - (full_n_cmpds * 91.25)
-    cmpdd_day_list = [91.25 for _ in range(full_n_cmpds)]
-    cmpdd_day_list.append(remain_cmpd_days)
+    get_day_cmpnds = calc_freq(depo_dict["scheme"])
+    day_cmpnds = get_day_cmpnds["cum_days"]
 
-    print("cmpdd_day_list", cmpdd_day_list)
+    full_n_cmpnds = floor(day_period/day_cmpnds) # in the case of quarterly compundsing 30x4
+    remain_cmpnd_days = day_period - (full_n_cmpnds * day_cmpnds)
+    cmpnd_days_list = [day_cmpnds for _ in range(full_n_cmpnds)]
+    cmpnd_days_list.append(remain_cmpnd_days)
 
-    maturity_val = pri
-    mat_list = []
-    # insert calcs
-    for qdays in cmpdd_day_list:
+    month_interest_li = []
+    month_maturity_li = []
+    maturity_val = principal
+    for qdays in cmpnd_days_list:
 
-        monthly_interest = (daily_irate * maturity_val)* qdays
-        print("monthly_interest", monthly_interest)
-        maturity_val = maturity_val + monthly_interest
-        mat_list.append(maturity_val)
+        month_interest = (daily_irate * maturity_val)* qdays
+        month_interest_li.append(month_interest)
+        print("month_interest", month_interest)
 
-    print("\nmat_list", mat_list)
-    print("\ndiff", round(depo_dict["ref_maturity_Amount"] - maturity_val, 2))
-    print("\ndiff", round(depo_dict["ref_maturity_Amount"] - mat_list[-1], 2))
+        maturity_val = maturity_val + month_interest
+        month_maturity_li.append(maturity_val)
+
+    print("cmpnd_days_list", cmpnd_days_list)
+    print("\nmonth_interest_li", month_interest_li)
+    print("\nmonth_maturity_li", month_maturity_li)
+
+    return {"idaily_maturity": month_maturity_li[-1],
+            "cmpnd_days_list": cmpnd_days_list,
+            "month_interest_li": month_interest_li,
+            "month_maturity_li": month_maturity_li}
 
 
 
@@ -80,71 +103,74 @@ def calculate_daily_maturity(depo_dict):
 
 
 
+if __name__ == "__main__":
+    for filename in sys.argv[1:]:
+        with open(filename, 'r') as FHO:
 
+            all_deposits = {}
+            for line in FHO:
+                print(line)
 
-for filename in sys.argv[1:]:
-    with open(filename, 'r') as FHO:
+                split_line = line.split(",")
 
-        all_deposits = {}
-        for line in FHO:
-            print(line)
+                print(len(split_line), split_line)
 
-            split_line = line.split(",")
-
-            print(len(split_line), split_line)
-
-            deposit_item = {}
-            for idx, value in enumerate(split_line):
-                print("xx", idx, value)
-                if value == "":
-                    pass
-                else:
-                    if idx == 2:
-                        dkey= "start_date"
-                        deposit_item[dkey] = str(value)
-                    elif idx == 3:
-                        dkey= "interest_rate"
-                        deposit_item[dkey] = float(value)
-                    elif idx == 4:
-                        dkey= "period"
-                        month_value = value.split(" ")
-                        deposit_item[dkey] = int(month_value[0])
-                    elif idx == 5:
-                        dkey= "ref_end_date"
-                        # calculate
-                        deposit_item[dkey] = str(value)
-                    elif idx == 6:
-                        dkey= "initial_deposit"
-                        deposit_item[dkey] = float(value)
-                    elif idx == 7:
-                        dkey= "ref_maturity_Amount"
-                        # calculate
-                        deposit_item[dkey] = float(value)
-                    elif idx == 8:
-                        dkey= "scheme"
-                        deposit_item[dkey] = str(value)
-                    elif idx == 9:
-                        dkey= "acc_no"
-                        deposit_item[dkey] = str(value).strip("\n")
+                deposit_item = {}
+                for idx, value in enumerate(split_line):
+                    print("xx", idx, value)
+                    if value == "":
+                        pass
                     else:
-                        print("hello")
+                        if idx == 2:
+                            dkey= "start_date"
+                            deposit_item[dkey] = str(value)
+                        elif idx == 3:
+                            dkey= "interest_rate"
+                            deposit_item[dkey] = float(value)
+                        elif idx == 4:
+                            dkey= "period"
+                            month_value = value.split(" ")
+                            deposit_item[dkey] = int(month_value[0])
+                        elif idx == 5:
+                            dkey= "ref_end_date"
+                            # calculate
+                            deposit_item[dkey] = str(value)
+                        elif idx == 6:
+                            dkey= "initial_deposit"
+                            deposit_item[dkey] = float(value)
+                        elif idx == 7:
+                            dkey= "ref_maturity_Amount"
+                            # calculate
+                            deposit_item[dkey] = float(value)
+                        elif idx == 8:
+                            dkey= "scheme"
+                            deposit_item[dkey] = str(value)
+                        elif idx == 9:
+                            dkey= "acc_no"
+                            deposit_item[dkey] = str(value).strip("\n")
+                        else:
+                            print("hello")
 
-            ##
-            print(deposit_item)
-            recalc_depo = calculate_maturity(deposit_item)
-            print(recalc_depo)
+                ##
+                print(deposit_item)
+                aprox_maturity_val = calc_approx_maturity(deposit_item)
 
-            print("\n################")
-            recalc_depo2 = calculate_daily_maturity(deposit_item)
-            sys.exit(0)
+                print_calc_verific("aprox_maturity_calc", aprox_maturity_val, deposit_item["ref_maturity_Amount"])
 
-            ## file
-            dep_name = deposit_item["acc_no"]
-            all_deposits[dep_name] = deposit_item
+                #################################################
+                print("\n################")
+                idaily_maturity_dic = calculate_daily_maturity(deposit_item)
 
-        for key, value in all_deposits.items():
-            print(key, value)
+                print_calc_verific("idaily_maturity", idaily_maturity_dic["idaily_maturity"], deposit_item["ref_maturity_Amount"])
+                sys.exit(0)
 
-            #sys.exit(0)
+                ## file
+                dep_name = deposit_item["acc_no"]
+                all_deposits[dep_name] = deposit_item
+
+            for key, value in all_deposits.items():
+                print(key, value)
+
+                #sys.exit(0)
 
 
